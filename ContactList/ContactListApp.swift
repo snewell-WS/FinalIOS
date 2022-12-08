ed80bcd1740e3c3b70d31fdaa739ebb181a1397c
snewell-WS <snewell234@gmail.com>
2022-12-07 18:53:02 -0600

    Initial Commit

---
+++ ContactList/ContactListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListApp.swift
+//  ContactList
+//
+//  Created by Sean Newell on 12/7/22.
+//
+
+import SwiftUI
+
+@main
+struct ContactListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
